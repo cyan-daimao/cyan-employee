34c20fcbca3c7170741834e127ff56944bb2ebef
cy.Y <daimao2817@gmail.com>
2026-05-21 23:04:28 +0800

    refactor: MapstructConvert 从 arch-common 迁移至 arch-base，添加 @Component 注解，统一替换 import 引用

--- cyan-employee-application/src/main/java/com/cyan/employee/adapter/http/department/convert/DepartmentAdapterConvert.java
+++ cyan-employee-application/src/main/java/com/cyan/employee/adapter/http/department/convert/DepartmentAdapterConvert.java
@@ -1,6 +1,6 @@
 package com.cyan.employee.adapter.http.department.convert;
 
-import com.cyan.arch.common.mapstruct.MapstructConvert;
+import com.cyan.arch.base.mapstruct.MapstructConvert;
 import com.cyan.employee.adapter.http.department.dto.DepartmentDTO;
 import com.cyan.employee.application.department.bo.DepartmentBO;
 import org.mapstruct.Mapper;
--- cyan-employee-application/src/main/java/com/cyan/employee/adapter/rpc/convert/EmployeeRPCConvert.java
+++ cyan-employee-application/src/main/java/com/cyan/employee/adapter/rpc/convert/EmployeeRPCConvert.java
@@ -1,6 +1,6 @@
 package com.cyan.employee.adapter.rpc.convert;
 
-import com.cyan.arch.common.mapstruct.MapstructConvert;
+import com.cyan.arch.base.mapstruct.MapstructConvert;
 import com.cyan.employee.client.query.EmployeeRPCListQuery;
 import com.cyan.employee.domain.employee.query.EmployeeListQuery;
 import org.mapstruct.Mapper;
